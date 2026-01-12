3858857cb51eef47a1008c55f8d58b34b9861355
Cody Owens <cowens1435@gmail.com>
2026-01-12 12:23:12 -0700

    Adding Project Files

---
+++ Assets/Agent/Scripts/AI_Agent.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public abstract class AI_Agent : MonoBehaviour
+{
+}
---
+++ Assets/Agent/Scripts/Autonomous_Agent.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Autonomous_Agent : AI_Agent
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
